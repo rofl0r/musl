23ae8be2dd31765fa3b2cd75ccf48f18d9b001b6
rofl0r <retnyg@gmx.net>
2014-01-07 23:19:26 +0100

    x32: fix some bits headers where kernel and userspace long mismatch

--- arch/x32/bits/msg.h
+++ arch/x32/bits/msg.h
@@ -4,10 +4,10 @@ struct msqid_ds
 	time_t msg_stime;
 	time_t msg_rtime;
 	time_t msg_ctime;
-	unsigned long msg_cbytes;
+	unsigned long long msg_cbytes;
 	msgqnum_t msg_qnum;
 	msglen_t msg_qbytes;
 	pid_t msg_lspid;
 	pid_t msg_lrpid;
-	unsigned long __unused[2];
+	unsigned long long __unused[2];
 };

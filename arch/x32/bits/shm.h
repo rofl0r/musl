f1e807063f18a410a600c79178938869490f1149
rofl0r <retnyg@gmx.net>
2014-01-07 22:53:38 +0100

    x32 port (diff against vanilla x86_64)

--- arch/x32/bits/shm.h
+++ arch/x32/bits/shm.h
@@ -9,7 +9,19 @@ struct shmid_ds
 	time_t shm_ctime;
 	pid_t shm_cpid;
 	pid_t shm_lpid;
-	unsigned long shm_nattch;
-	unsigned long __pad1;
-	unsigned long __pad2;
+	unsigned long long shm_nattch;
+	unsigned long long __pad1;
+	unsigned long long __pad2;
 };
+
+struct shminfo {
+	unsigned long long shmmax, shmmin, shmmni, shmseg, shmall, __unused[4];
+};
+
+struct shm_info {
+	int __used_ids;
+	unsigned long long shm_tot, shm_rss, shm_swp;
+	unsigned long long __swap_attempts, __swap_successes;
+};
+
+typedef unsigned long long shmatt_t;

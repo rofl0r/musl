2a70d4202e849a1243634f1bcd830ec3da421f35
rofl0r <retnyg@gmx.net>
2014-02-21 20:33:58 +0100

    x32: add SYS_futex to syscall4-5
    using the syscall6 dispatch for all uses would bloat i386.

--- arch/x32/syscall_arch.h
+++ arch/x32/syscall_arch.h
@@ -53,6 +53,12 @@ static __inline long __syscall4(long long n, long long a1, long long a2, long lo
 	unsigned long ret;
 	register long long r10 __asm__("r10") = a4;
 	switch (n) {
+	case SYS_futex:
+		if(a2 == 0 /* FUTEX_WAIT*/) {
+			if(r10) r10 = (unsigned long) (&(struct __timespec_kernel) {
+			.tv_sec = __tsc(r10)->tv_sec, .tv_nsec = __tsc(r10)->tv_nsec});
+		}
+		break;
 	case SYS_utimensat:
 		if(a3) a3 = (unsigned long) ((struct __timespec_kernel[2]) {
 		[0] = {.tv_sec = __tsc(a3)[0].tv_sec, .tv_nsec = __tsc(a3)[0].tv_nsec},
@@ -76,6 +82,12 @@ static __inline long __syscall5(long long n, long long a1, long long a2, long lo
 	register long long r10 __asm__("r10") = a4;
 	register long long r8 __asm__("r8") = a5;
 	switch (n) {
+	case SYS_futex:
+		if(a2 == 0 /* FUTEX_WAIT*/) {
+			if(r10) r10 = (unsigned long) (&(struct __timespec_kernel) {
+			.tv_sec = __tsc(r10)->tv_sec, .tv_nsec = __tsc(r10)->tv_nsec});
+		}
+		break;
 	case SYS_mq_timedsend: case SYS_mq_timedreceive:
 		if(r8) r8 = (unsigned long) (&(struct __timespec_kernel) {
 		.tv_sec = __tsc(r8)->tv_sec, .tv_nsec = __tsc(r8)->tv_nsec});
